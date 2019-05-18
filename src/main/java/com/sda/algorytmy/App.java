8a93f2704677a1539aa795ef5d77218de406b55e
bart7801 <bart7801@o2.pl>
2019-05-18 12:13:53 +0100

    wersja finalna wraz z rysunkiem

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -10,37 +10,23 @@ public class App
 {
 
     public static void main( String[] args )
-
     {
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Podaj pierwszy wyraz ciagu: ");
-
-        int a = scanner.nextInt();
-
-        System.out.println("Podaj ilosc wyrazow ciagu: ");
-
-        int n = scanner.nextInt();
-
+        int a1 = scanner.nextInt();
         System.out.println("Podaj przyrost ciagu: ");
-
         int d = scanner.nextInt();
-
-        System.out.print(a + ", ");
+        System.out.println("Podaj ilosc wyrazow ciagu: ");
+        int n = scanner.nextInt();
+        System.out.print(a1 + ", ");
 
         int next;
 
+        for(int i = 2; i <=n; i++){
 
-
-        for(int i = 2; i <= n; i++){
-
-            next = a + (i - 1) * d;
+            next = a1 + (i - 1) * d;
 
             System.out.print(next + ", ");
-
         }
-
     }
-
 }
